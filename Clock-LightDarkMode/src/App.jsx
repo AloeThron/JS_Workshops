2a1cc3d4ce56859adb70206e674e4684f211c7d8
AloeThron <lxe.tr.deat@gmail.com>
2023-12-07 02:19:23 +0700

    update

--- Clock-LightDarkMode/src/App.jsx
+++ Clock-LightDarkMode/src/App.jsx
@@ -1,24 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import DarkModeToggle from "./components/DarkModeToggle";
 
 function App() {
-
   const [time, setTime] = useState(new Date());
   const [hours, setHours] = useState(time.getHours());
   const [minutes, setMinuts] = useState(time.getMinutes());
   const [seconds, setSeconds] = useState(time.getSeconds());
 
   setInterval(() => {
-    setTime(new Date())
-    setHours(time.getHours())
-    setMinuts(time.getMinutes())
-    setSeconds(time.getSeconds())
-  }, 2000);
+    setSeconds(new Date().getSeconds());
+  }, 1000);
+
+  useEffect(() => {
+    setTime(new Date());
+    setHours(time.getHours());
+    setMinuts(time.getMinutes());
+    setSeconds(time.getSeconds());
+  }, [seconds]);
 
   return (
     <>
-      <div className="h-screen flex flex-col justify-center items-center dark:bg-black">
+      <div className="h-screen flex flex-col justify-center items-center transition-all duration-700 ease-out dark:bg-black dark:transition-all dark:duration-700 dark:ease-out">
         <div className="flex justify-center">
           <div id="time" className="text-6xl font-bold dark:text-white">
             {hours < 10 ? `0${hours}` : hours}:
