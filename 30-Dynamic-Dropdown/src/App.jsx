52284ec1627b150d33dbb146a840af3c866ed8aa
AloeThron <lxe.tr.deat@gmail.com>
2023-12-13 16:50:40 +0700

    coding

---
+++ 30-Dynamic-Dropdown/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./App.css";
+import Card from "./components/card";
+import Zoom from "./components/Zoom";
+
+function App() {
+  const list = [1, 2, 3, 4, 5, 6, 7, 8];
+  const [vis, setVis] = useState("hidden");
+  const [img, setImg] = useState("");
+
+  const link = list.map((n) => {
+    return `https://raw.githubusercontent.com/AloeThron/React_Workshops/main/Image-Popup/src/components/foods-images/small/s_${n}.jpg`;
+  });
+
+  return (
+    <>
+      <div>
+        <div className="m-10 flex justify-center box-content">
+          <div className="flex flex-wrap md:gap-10 justify-center 2xl:justify-around">
+            {link.map((m) => (
+              <div key={m}>
+                <Card name={m} setVis={setVis} setImg={setImg} />
+              </div>
+            ))}
+          </div>
+        </div>
+        <Zoom vis={vis} setVis={setVis} img={img} />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ 30-Dynamic-Dropdown/src/components/Card.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function card(props) {
+
+  return (
+    <div>
+      <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-xs lg:max-w-sm">
+        <img className="h-80 rounded-t-lg bg-cover bg-center" src={props.name} />
+        <div className="p-5">
+          <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">
+            Noteworthy technology acquisitions 2021
+          </h5>
+          <p className="font-normal text-gray-700 mb-3">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+
+            <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center" onClick={() => {
+                props.setVis("")
+                props.setImg(props.name)
+            }}>Zoom</button>
+        </div>
+      </div>
+    </div>
+  );
+}
