1b69570ec5b5d940285f1da1e3c6901637239350
AloeThron <lxe.tr.deat@gmail.com>
2023-12-15 02:52:25 +0700

    update code

--- 31-Infinite-Scroll/src/App.jsx
+++ 31-Infinite-Scroll/src/App.jsx
@@ -4,6 +4,10 @@ import "./App.css";
 
 function App() {
   const [images, setImages] = useState([]);
+  const apiKey = `zp0BGJGUJtBhrTEzacC0UXzyjQ5M2efitpF7Vzo5xBE`;
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     let nums = [];
@@ -11,6 +15,40 @@ function App() {
       nums.push({ id: i, imageUrl: `https://source.unsplash.com/random/${i}` });
     }
     setImages(nums);
+  }, []);
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     setIsLoading(true);
+  //     try {
+  //       const res = await fetch(
+  //         `https://api.unsplash.com/photos/?client_id=${apiKey}&page=${page}`
+  //       );
+  //       const data = res.json();
+  //       setPhotos((oldData) => {
+  //         return [...oldData, ...data];
+  //       });
+  //     } catch (error) {
+  //       console.log("Fetching data is error: ", error);
+  //     }
+  //   }
+  //   fetchData();
+  // }, [page]);
+
+  useEffect(() => {
+    const event = window.addEventListener("scroll", () => {
+      if (
+        window.innerHeight + window.scrollY >
+          document.body.offsetHeight - 500 &&
+        !isLoading
+      ) {
+        setPage((oldPage) => {
+          return oldPage + 1;
+        });
+      }
+    });
+    return () => window.removeEventListener("scroll", event);
+    // eslint-disable-next-line
   }, []);
 
   return (
@@ -22,11 +60,10 @@ function App() {
       <div className="flex flex-wrap sm:gap-10 justify-center 2xl:justify-around">
         {images.map((image) => (
           <div key={image.id}>
-            <Card  id={image.id} img={image.imageUrl} />
+            <Card id={image.id} img={image.imageUrl} />
           </div>
         ))}
       </div>
-
     </div>
   );
 }
